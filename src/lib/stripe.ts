2c480e1368598df558f9894bece8f50c98d2cce6
Prashant Singh <prashanttt001@gmail.com>
2026-07-04 10:22:56 +0530

    removed payment link for deployment: temporary

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy_key_to_bypass_build_error', {
   apiVersion: '2025-02-24.acacia' as any,
   typescript: true,
 })
